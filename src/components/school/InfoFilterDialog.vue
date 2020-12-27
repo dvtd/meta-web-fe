8e0962b4d7d5f2dc0c7e637ba9c20fc35ace4942
Duyntkd <duyntkd@gmail.com>
2020-12-27 21:36:08 +0700

    * Edited delete Dialog * Edit and New dialog not done yet

---
+++ src/components/school/InfoFilterDialog.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-dialog
+    v-model="isDialogOpen"
+    max-width="500px"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        color="success"
+        dark
+        class="mb-2"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon color="white" x-large>mdi-account-multiple-plus-outline</v-icon>
+        School
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">{{ formTitle }}</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col
+              cols="12"
+              sm="6"
+              md="4"
+            >
+              <v-text-field
+                v-model="editedItem.Name"
+                label="Name"
+              ></v-text-field>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+              md="4"
+            >
+              <v-text-field
+                v-model="editedItem.Representative"
+                label="Representative"
+              ></v-text-field>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+              md="4"
+            >
+              <v-text-field
+                v-model="editedItem.ContractInfo"
+                label="ContractInfo"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="blue darken-1"
+          text
+          @click="$emit('close')"
+        >
+          Cancel
+        </v-btn>
+        <v-btn
+          color="blue darken-1"
+          text
+          @click="$emit('save')"
+        >
+          Save
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    formTitle: String,
+    isDialogOpen: Boolean,
+    editedItem: Object
+  },
+  data () {
+
+  }
+}
+</script>
--- src/components/school/SchoolGrid.vue
+++ src/components/school/SchoolGrid.vue
@@ -15,112 +15,8 @@
         vertical
       ></v-divider>
       <v-spacer></v-spacer>
-      <v-dialog
-        v-model="dialog"
-        max-width="500px"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            color="success"
-            dark
-            class="mb-2"
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon color="white" x-large>mdi-account-multiple-plus-outline</v-icon>
-            School
-          </v-btn>
-        </template>
-        <v-card>
-          <v-card-title>
-            <span class="headline">{{ formTitle }}</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-                >
-                  <v-text-field
-                    v-model="editedItem.Id"
-                    label="Id"
-                  ></v-text-field>
-                </v-col>
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-                >
-                  <v-text-field
-                    v-model="editedItem.Name"
-                    label="Name"
-                  ></v-text-field>
-                </v-col>
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-                >
-                  <v-text-field
-                    v-model="editedItem.Representative"
-                    label="Representative"
-                  ></v-text-field>
-                </v-col>
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-                >
-                  <v-text-field
-                    v-model="editedItem.LocationId"
-                    label="LocationId"
-                  ></v-text-field>
-                </v-col>
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-                >
-                  <v-text-field
-                    v-model="editedItem.ContractInfo"
-                    label="ContractInfo"
-                  ></v-text-field>
-                </v-col>
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-                >
-                  <v-text-field
-                    v-model="editedItem.IsDeleted"
-                    label="IsDeleted"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              color="blue darken-1"
-              text
-              @click="close"
-            >
-              Cancel
-            </v-btn>
-            <v-btn
-              color="blue darken-1"
-              text
-              @click="save"
-            >
-              Save
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+      <info-filter-dialog :is-dialog-open="dialog" :edited-item="editedItem" :form-title="formTitle" @close="close" @save="save"></info-filter-dialog>
+      <delete-dialog :is-dialog-open="dialogDelete" @close="closeDelete" @confirm="deleteItemConfirm"></delete-dialog>
     </div>
     <v-card>
       <v-tabs
@@ -142,24 +38,6 @@
               sort-by="calories"
               class="elevation-1"
             >
-              <template v-slot:top>
-                <v-toolbar
-                  flat
-                >
-
-                  <v-dialog v-model="dialogDelete" max-width="500px">
-                    <v-card>
-                      <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                        <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                        <v-spacer></v-spacer>
-                      </v-card-actions>
-                    </v-card>
-                  </v-dialog>
-                </v-toolbar>
-              </template>
               <template v-slot:item.actions="{ item }">
                 <v-icon
                   small
@@ -193,7 +71,10 @@
 
 <script>
 
+import DeleteDialog from './DeleteDialog'
+import InfoFilterDialog from './InfoFilterDialog'
 export default {
+  components: { 'info-filter-dialog': InfoFilterDialog, 'delete-dialog': DeleteDialog },
   props: {
     headerData: {
       type: Array,
