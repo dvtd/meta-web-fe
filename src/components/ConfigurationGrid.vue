a8b30b584764d5dc10f1400b0c89e3db11802d2a
khoaNN <khoann1999@gmail.com>
2021-01-02 20:55:23 +0700

    [KhoaNN] Create System Configuration

---
+++ src/components/ConfigurationGrid.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app id="inspire">
+    <v-card class="mx-10">
+      <v-toolbar color="#262261" dark>
+        <v-icon> mdi-cogs </v-icon>
+
+        <v-toolbar-title>Settings</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-toolbar>
+
+      <v-list flat subheader three-line>
+        <v-subheader>Features</v-subheader>
+        <v-divider></v-divider>
+        <v-list-item-group v-model="settings" multiple active-class="">
+          <v-list-item>
+            <template v-slot:default="{ active }">
+              <v-list-item-action>
+                <v-checkbox :input-value="active"></v-checkbox>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Notifications</v-list-item-title>
+                <v-list-item-subtitle
+                  >Enable notification feature in system</v-list-item-subtitle
+                >
+              </v-list-item-content>
+            </template>
+          </v-list-item>
+
+          <v-list-item>
+            <template v-slot:default="{ active }">
+              <v-list-item-action>
+                <v-checkbox :input-value="active"></v-checkbox>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Attendance</v-list-item-title>
+                <v-list-item-subtitle>Enable attendace feature</v-list-item-subtitle>
+              </v-list-item-content>
+            </template>
+          </v-list-item>
+
+          <v-list-item>
+            <template v-slot:default="{ active }">
+              <v-list-item-action>
+                <v-checkbox :input-value="active"></v-checkbox>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Request</v-list-item-title>
+                <v-list-item-subtitle>Enable request feature</v-list-item-subtitle>
+              </v-list-item-content>
+            </template>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <v-toolbar color="blue darken-4" dark>
+        <v-btn color="green" class="ma-2 white--text"> Save </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="red" class="ma-2 white--text"> Cancel </v-btn>
+      </v-toolbar>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- src/router/router.js
+++ src/router/router.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 import Index from '../views/Index.vue'
 import RequestList from '../views/RequestList'
 import RequestDetails from '@/views/RequestDetails'
-
+import Configuration from '@/views/Configuration'
 Vue.use(VueRouter)
 
 const routes = [
@@ -22,6 +22,11 @@ const routes = [
         path: 'requestdetail',
         name: 'RequestDetails',
         component: RequestDetails
+      },
+      {
+        path: 'configuration',
+        name: 'configuration',
+        component: Configuration
       }
     ]
   }
